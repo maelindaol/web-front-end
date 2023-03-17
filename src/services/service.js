37c7020342d8ffcf5b1901c65108e1cd1b1581a7
maelindaol <72915659+maelindaol@users.noreply.github.com>
2023-03-17 15:07:30 -0600

    Add files via upload

---
+++ src/services/service.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+let link = "app-b8c6868d-af05-422d-b256-d4d1285370cb.cleverapps.io";
+
+async function GetAll() {
+  const data = await axios.get(`http://${link}/tasks`);
+  return data.data;
+}
+
+async function InsertTask(title, description, date) {
+  try {
+    await axios.post(`http://${link}/tasks`, {
+      title,
+      description,
+      date,
+    });
+    return false;
+  } catch (error) {
+    console.log("error insert");
+    return true;
+  }
+}
+
+async function DeleteTask(id) {
+  try {
+    await axios.delete(`http://${link}/tasks` + id);
+  } catch (error) {
+    console.log("error delete");
+    return true;
+  }
+}
+
+async function UpdateTask(id, title, description) {
+  try {
+    await axios.put(`http://${link}/tasks` + id, {
+      title,
+      description,
+    });
+  } catch (error) {
+    console.log("error update");
+    return true;
+  }
+}
+
+export { GetAll, InsertTask, DeleteTask, UpdateTask };
